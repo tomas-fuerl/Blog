ed1a05d3c1280e5c6b57c187eb875b129957bd8f
Tomas Fürl <tomasfurl@MacBook-Pro.local>
2020-07-11 14:06:36 +0200

    Zeige bei Pfaden dinge, ansonsten die Startseite

--- htmlFile.js
+++ htmlFile.js
@@ -4,12 +4,37 @@ const fs = require('fs').promises;
 const host = 'localhost';
 const port = 8000;
 
+const books = JSON.stringify([
+    { title: "The Alchemist", author: "Paulo Coelho", year: 1988 },
+    { title: "The Prophet", author: "Kahlil Gibran", year: 1923 }
+]);
+
+const authors = JSON.stringify([
+    { name: "Paulo Coelho", countryOfBirth: "Brazil", yearOfBirth: 1947 },
+    { name: "Kahlil Gibran", countryOfBirth: "Lebanon", yearOfBirth: 1883 }
+]);
+
 let indexFile;
 
 const requestListener = function (req, res) {
-    res.setHeader("Content-Type", "text/html");
-    res.writeHead(200);
-    res.end(indexFile);
+
+    switch (req.url) {
+        case "/books.json":
+            res.setHeader("Content-Type", "application/json");
+            res.writeHead(200);
+            res.end(books);
+            break
+        case "/authors":
+            res.setHeader("Content-Type", "application/json");
+            res.writeHead(200);
+            res.end(authors);
+            break
+        default:
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(indexFile);
+    }
+
 };
 
 const server = http.createServer(requestListener);
