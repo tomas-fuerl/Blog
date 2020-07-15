1bfe7bf57627cf3babc6058bcd1c1cb9faad8e80
Tomas Fürl <tomasfurl@MacBook-Pro.local>
2020-07-15 20:38:37 +0200

    linted existing code

--- serverstart.js
+++ serverstart.js
@@ -1,39 +1,36 @@
 /* This file is for starting the server and the serverlogic */
 
-const http = require("http");
+const http = require('http');
 const fs = require('fs').promises;
 
 const host = 'localhost';
 const port = 8000;
-
 let indexFile;
 
-const requestListener = function (req, res) {
-
-    switch (req.url) {
-        case "/":
-            res.setHeader("Content-Type", "text/html");
-            res.writeHead(200);
-            res.end(indexFile);
-            break
-        default:
-            res.setHeader("Content-Type", "text/html");
-            res.writeHead(404);
-            res.end("error");
-    }
-
+const requestListener = function(req, res) {
+  switch (req.url) {
+    case '/':
+      res.setHeader('Content-Type', 'text/html');
+      res.writeHead(200);
+      res.end(indexFile);
+      break;
+    default:
+      res.setHeader('Content-Type', 'text/html');
+      res.writeHead(404);
+      res.end('error');
+  }
 };
 
 const server = http.createServer(requestListener);
 
-fs.readFile(__dirname + "/index.html")
-    .then(contents => {
-        indexFile = contents;
-        server.listen(port, host, () => {
-            console.log(`Server is running on http://${host}:${port}`);
-        });
+fs.readFile(__dirname + '/index.html')
+    .then((contents) => {
+      indexFile = contents;
+      server.listen(port, host, () => {
+        console.log(`Server is running on http://${host}:${port}`);
+      });
     })
-    .catch(err => {
-        console.error(`Could not read index.html file: ${err}`);
-        process.exit(1);
+    .catch((err) => {
+      console.error(`Could not read index.html file: ${err}`);
+      process.exit(1);
     });
