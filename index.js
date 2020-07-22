14a5ef63985f09b2676520a167767f3c4a4e08aa
Tomas Fürl <tomas@fuerl-online.de>
2020-07-22 16:13:12 +0200

    fontawesome and bootstrap 4.5.0 npm

--- index.js
+++ index.js
@@ -8,8 +8,20 @@ server.listen(port, function() {
   console.log(`Server is running on http://${host}:${port}`);
 });
 
+/* serve the public folder */
 app.use(express.static(__dirname + '/public'));
 
+/* serve the font awesome css */
+app.use(
+    express.static(__dirname + '/node_modules/@fortawesome/fontawesome-free/'),
+);
+
+/* serve the bootstrap files */
+app.use(
+    express.static(__dirname + '/node_modules/bootstrap/dist/'),
+);
+
+/* serve the error page */
 app.use(function(req, res, next) {
   res.status(404).sendFile(__dirname + '/public/404.html');
 });
