af3c92ba06fc7b2c286a092f761f002a6ee84aaa
Tomas Fürl <tomas@fuerl-online.de>
2020-07-23 17:47:24 +0200

    languages from JSON

--- private/scripts/index.ts
+++ private/scripts/index.ts
@@ -1,17 +1,70 @@
-class UserLanguage {
+class Language {
     #language: string;
+    #languageJSON;
 
     constructor() {
-        this.setLanguage ();
+        this.setLanguage();
+        this.setLanguageJSON(this.getLanguage());
     }
-    setLanguage () {
-        this.#language = "de-DE";
+
+    /* sets #language to a value */
+    setLanguage() {
+        if (this.getStorageLanguage() === null) {
+            /* navigator.language = browser language */
+            this.setStorageLanguage(navigator.language);
+        }
+        this.#language = this.getStorageLanguage();
     }
-    getLanguage () {
+
+    getLanguage() {
         return this.#language;
+    }
+
+    setStorageLanguage(lang) {
+        window.localStorage.setItem('Language', lang);
+        console.log('language was defined and safed in local storage');
+    }
+
+    /* returns null if item not defined */
+    getStorageLanguage() {
+        return window.localStorage.getItem('Language');
+    }
+
+    deleteStorageLanguage() {
+        window.localStorage.removeItem('Language');
+    }
+
+    setLanguageJSON(lang) {
+        const url = `http://localhost:8080/language/${lang}.json`;
+        let languageObject;
+
+        const xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                const myObj = this.responseText;
+                languageObject = myObj;
+            }
+        };
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
+        console.log(languageObject)
+        this.#languageJSON = languageObject;
+    }
+
+    getLanguageJSON() {
+        return this.#languageJSON;
+    }
+
+    /* Channges the language to a certain value in the local storage and this class */
+    changeLanguage(lang) {
+        this.setStorageLanguage(lang);
+        this.setLanguage();
+        this.setLanguageJSON(this.getLanguage());
     }
 }
 
-const myLanguage = new UserLanguage();
+const languagehandling = new Language();
 
-alert(myLanguage.getLanguage());
+console.log(languagehandling.getLanguageJSON());
+const languageFile = languagehandling.getLanguageJSON();
+console.log(languageFile.hello);
