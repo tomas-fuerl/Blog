7ec93d512bcee26f899468a7d91ec3f2d8f9b09b
Tomas Fürl <tomas@fuerl-online.de>
2020-07-24 08:52:35 +0200

    commas at the end of code always

--- private/scripts/index.ts
+++ private/scripts/index.ts
@@ -3,68 +3,68 @@ class Language {
     #languageJSON;
 
     constructor () {
-      this.setLanguage()
-      this.setLanguageJSON(this.getLanguage())
+      this.setLanguage();
+      this.setLanguageJSON(this.getLanguage());
     }
 
     /* sets #language to a value */
     setLanguage () {
       if (this.getStorageLanguage() === null) {
         /* navigator.language = browser language */
-        this.setStorageLanguage(navigator.language)
+        this.setStorageLanguage(navigator.language);
       }
-      this.#language = this.getStorageLanguage()
+      this.#language = this.getStorageLanguage();
     }
 
     getLanguage () {
-      return this.#language
+      return this.#language;
     }
 
     setStorageLanguage (lang) {
-      window.localStorage.setItem('Language', lang)
-      console.log('language was defined and safed in local storage')
+      window.localStorage.setItem('Language', lang);
+      console.log('language was defined and safed in local storage');
     }
 
     /* returns null if item not defined */
     getStorageLanguage () {
-      return window.localStorage.getItem('Language')
+      return window.localStorage.getItem('Language');
     }
 
     deleteStorageLanguage () {
-      window.localStorage.removeItem('Language')
+      window.localStorage.removeItem('Language');
     }
 
     setLanguageJSON (lang) {
-      const url = `http://localhost:8080/language/${lang}.json`
-      let languageObject
+      const url = `http://localhost:8080/language/${lang}.json`;
+      let languageObject;
 
-      const xmlhttp = new XMLHttpRequest()
+      const xmlhttp = new XMLHttpRequest();
       xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-          const myObj = this.responseText
-          languageObject = myObj
+          const myObj = this.responseText;
+          languageObject = myObj;
         }
-      }
-      xmlhttp.open('GET', url, true)
-      xmlhttp.send()
-      console.log(languageObject)
-      this.#languageJSON = languageObject
+      };
+      xmlhttp.open('GET', url, true);
+      xmlhttp.send();
+      console.log(languageObject);
+      this.#languageJSON = languageObject;
     }
 
     getLanguageJSON () {
-      return this.#languageJSON
+      return this.#languageJSON;
     }
 
     /* Channges the language to a certain value in the local storage and this class */
     changeLanguage (lang) {
-      this.setStorageLanguage(lang)
-      this.setLanguage()
-      this.setLanguageJSON(this.getLanguage())
+      this.setStorageLanguage(lang);
+      this.setLanguage();
+      this.setLanguageJSON(this.getLanguage());
     }
 }
 
-const languagehandling = new Language()
+const languagehandling = new Language();
 
-console.log(languagehandling.getLanguageJSON())
-const languageFile = languagehandling.getLanguageJSON()
-console.log(languageFile.hello)
+console.log(languagehandling.getLanguageJSON());
+const languageFile = languagehandling.getLanguageJSON();
+console.log(languageFile.hello);
